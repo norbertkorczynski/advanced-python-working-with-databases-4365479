faa6c2609542c67f82205b35be7f7851dec20ff2
Norbert Korczyński <73656711+norbertkorczynski@users.noreply.github.com>
2023-12-30 22:31:18 +0000

    Done

--- sqlalchemy-workspace/database.py
+++ sqlalchemy-workspace/database.py
@@ -4,8 +4,8 @@ engine = sqlalchemy.create_engine('sqlite:///testDB.db', echo=True)
 
 metadata = sqlalchemy.MetaData()
 
-movies_table = sqlalchemy.Table("Users", metadata,
-    sqlalchemy.Column("User_Id", sqlalchemy.Integer),
+users_table = sqlalchemy.Table("Users", metadata,
+    sqlalchemy.Column("User_Id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("First Name", sqlalchemy.Text),
     sqlalchemy.Column("Last Name", sqlalchemy.Text),
     sqlalchemy.Column("Email", sqlalchemy.Text))
@@ -13,14 +13,15 @@ movies_table = sqlalchemy.Table("Users", metadata,
 metadata.create_all(engine)
 
 users = [
-    (1, "Roman", "Kolton", "rkolton@polsat.pl"),
-    (2, "Adam", "Nowak", "nowaczek@o2.pl"),
-    (3, "Norman", "James", "njames@gmail.com"),
-    (4, "Test", "User", "test.user@gmail.com"),
-    (5, "Dupa", "Romana", "email1@poczta.fm")]
+    {"First Name":"Roman", "Last Name":"Kolton", "Email":"rkolton@polsat.pl"},
+    {"First Name":"Adam", "Last Name":"Nowak", "Email":"nowaczek@o2.pl"},
+    {"First Name":"Norman", "Last Name":"James", "Email":"njames@gmail.com"},
+    {"First Name":"Test", "Last Name":"User", "Email":"test.user@gmail.com"},
+    {"First Name":"Dupa", "Last Name":"Romana", "Email":"email1@poczta.fm"}]
 
 with engine.connect() as conn:
-    conn.execute(movies_table.insert().values(users))
+    conn.execute(sqlalchemy.insert(users_table).values(users))
 
-    for row in conn.execute(sqlalchemy.select(movies_table)):
+    for row in conn.execute(sqlalchemy.select(users_table)):
         print(row)
+    conn.commit()
